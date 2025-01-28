a8eeecbbc4f33d5d952812d2c1df930a572d82d3
Rajangupta09 <rjnkumar05@gmail.com>
2025-01-28 17:57:28 +0530

    [Refactor] Enable Client Side LoadBalancing for service discovery

--- order-service/src/main/java/com/project/order_service/service/OrderService.java
+++ order-service/src/main/java/com/project/order_service/service/OrderService.java
@@ -21,8 +21,8 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
 
-    private final OrderRepository orderRepository;
-    private final WebClient       webClient;
+    private final OrderRepository         orderRepository;
+    private final WebClient.Builder       webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -37,8 +37,8 @@ public class OrderService {
                                         .toList();
 
         // Call Inventory Service, and place order if in stock
-        InventoryResponse[] inventoryResponses = webClient.get()
-                                  .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                                  .uri("http://inventory-service/api/inventory",
                                           uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                                   .retrieve()
                                   .bodyToMono(InventoryResponse[].class)
