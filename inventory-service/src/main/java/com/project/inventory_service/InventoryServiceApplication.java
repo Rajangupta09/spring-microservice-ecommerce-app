fc8838ea0dd3c6621ff2d61b3506701dc0636ef7
Rajangupta09 <rjnkumar05@gmail.com>
2025-01-28 17:34:05 +0530

    [ADD] Eureka Service Discovery for host resolution

--- inventory-service/src/main/java/com/project/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/project/inventory_service/InventoryServiceApplication.java
@@ -1,13 +1,11 @@
 package com.project.inventory_service;
 
-import com.project.inventory_service.model.Inventory;
-import com.project.inventory_service.repository.InventoryRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
